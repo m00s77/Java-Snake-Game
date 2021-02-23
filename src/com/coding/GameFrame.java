5af5b70c18d26550fa44949531a26e68a7934774
m00s77 <65338149+m00s77@users.noreply.github.com>
2021-02-23 21:05:11 +0000

    Add files via upload

---
+++ src/com/coding/GameFrame.java
@@ -0,0 +1,30 @@
+package com.coding;
+
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+
+    GameFrame() {
+
+        setUndecorated(true);
+
+        add(new GamePanel());
+        setTitle("Snake");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        pack();
+        setVisible(true);
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new GameFrame();
+            }
+        });
+    }
+
+
+}
